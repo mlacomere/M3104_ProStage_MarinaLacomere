8bd03b95967cd9d9eb17e23b8d27e5e8028a5b94
Marina Lacomere <mlacomere@iutbayonne.univ-pau.fr>
2019-12-04 09:31:41 +0100

    Ajout de la table Stage et ses relations

---
+++ src/Entity/Stage.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\StageRepository")
+ */
+class Stage
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $descriptionMission;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mailContact;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $personneAContacter;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateDebut;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateFin;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Formation", inversedBy="stages")
+     */
+    private $formations;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Entreprise", inversedBy="stages")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $entreprise;
+
+    public function __construct()
+    {
+        $this->formations = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescriptionMission(): ?string
+    {
+        return $this->descriptionMission;
+    }
+
+    public function setDescriptionMission(string $descriptionMission): self
+    {
+        $this->descriptionMission = $descriptionMission;
+
+        return $this;
+    }
+
+    public function getMailContact(): ?string
+    {
+        return $this->mailContact;
+    }
+
+    public function setMailContact(string $mailContact): self
+    {
+        $this->mailContact = $mailContact;
+
+        return $this;
+    }
+
+    public function getPersonneAContacter(): ?string
+    {
+        return $this->personneAContacter;
+    }
+
+    public function setPersonneAContacter(string $personneAContacter): self
+    {
+        $this->personneAContacter = $personneAContacter;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(?\DateTimeInterface $dateDebut): self
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(?\DateTimeInterface $dateFin): self
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Formation[]
+     */
+    public function getFormations(): Collection
+    {
+        return $this->formations;
+    }
+
+    public function addFormation(Formation $formation): self
+    {
+        if (!$this->formations->contains($formation)) {
+            $this->formations[] = $formation;
+        }
+
+        return $this;
+    }
+
+    public function removeFormation(Formation $formation): self
+    {
+        if ($this->formations->contains($formation)) {
+            $this->formations->removeElement($formation);
+        }
+
+        return $this;
+    }
+
+    public function getEntreprise(): ?Entreprise
+    {
+        return $this->entreprise;
+    }
+
+    public function setEntreprise(?Entreprise $entreprise): self
+    {
+        $this->entreprise = $entreprise;
+
+        return $this;
+    }
+}
