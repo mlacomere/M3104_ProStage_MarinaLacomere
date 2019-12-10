9b9980e4ac1def3da94e10d26230e5fcf95863fd
Marina Lacomere <mlacomere@iutbayonne.univ-pau.fr>
2019-12-10 18:28:01 +0100

    Ajout des jeux de tests formations en base de données

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,38 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Entreprise;
+use App\Entity\Stage;
+use App\Entity\Formation;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create('fr_FR');
+        
 
+        /*********************************
+         * ***Création des Formations ****
+        **********************************/
+
+        $dutInfo = new Formation();
+        $dutInfo -> setNom("DUT Informatique");
+
+        $dutTic = new Formation();
+        $dutTic -> setNom("DUT TIC");
+
+        $licenceProMultimedia = new Formation();
+        $licenceProMultimedia -> setNom("Licence Pro Multimedia");
+
+        $tableauFormations = array($dutInfo,$dutTic,$licenceProMultimedia);
+        foreach ($tableauFormations as $uneFormation){
+            $manager->persist($uneFormation);
+        }
+        
+        /*********************************
+         * ***Création des Entreprises ****
+        **********************************/
         $manager->flush();
     }
 }
