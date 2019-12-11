2ccbd06918734c83a9dd1b8fd297bcec9090de29
Marina Lacomere <mlacomere@iutbayonne.univ-pau.fr>
2019-12-11 13:53:49 +0100

    Création des jeux de données des stages liés

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -36,19 +36,50 @@ class AppFixtures extends Fixture
         /*********************************
          * ***Création des Entreprises ****
         **********************************/
-
-        for($i=0;$i<=10;$i++){
+        $tableauEntreprise=array();
+        for($i=1;$i<=10;$i++){
             $entreprise = new Entreprise();
             $entreprise -> setNom($faker->company());
             $entreprise -> setDescription($faker->realText($maxNbChars = 255, $indexSize = 2));
             $entreprise -> setAdresse($faker->address());
             $entreprise -> setSiteWeb($faker->url());
+            array_push($tableauEntreprise,$entreprise); 
+            // conservation des entreprises dans un tableau pour pouvoir y accéder et les lier avec un ou des stages
             $manager->persist($entreprise);
         }
 
         /*******************************************************************
          * ***Création des Stages et entreprises / formations associées ****
         *******************************************************************/
+        for($i=1;$i<=20;$i++){
+            $stage = new Stage();
+            $stage -> setTitre($faker->catchPhrase());
+            $stage -> setDescriptionMission($faker->realText($maxNbChars = 255, $indexSize = 2));
+            $stage -> setMailContact($faker->email());
+            $stage -> setPersonneAContacter($faker->name($gender ='male'|'female'));
+            $stage -> setDateDebut($faker->dateTimeBetween($startDate = 'now', $endDate = '+ 1years', $timezone = null, $format = 'd-m-Y'));
+            $stage -> setDateFin($faker->dateTimeBetween($startDate = $stage->getDateDebut(), $endDate = '+ 1years', $timezone = null, $format = 'd-m-Y'));
+            //lier les entreprises et formations!!
+            //1 stage est lié à une entreprise
+            //récupération aléatoire d'une entreprise du $tableauEntreprise 
+            $ent=$faker->randomElement($array = $tableauEntreprise);
+            $stage -> setEntreprise($ent);
+            //modification au niveau de l'entreprise
+            $ent -> addStage($stage);
+            //génération d'un nombre aléatoire entre 1 et 3 pour le nombre de formations liées à ce stage
+            $nbForm=$faker->numberBetween($min = 1, $max = 3);
+            for($j=1; $j<=$nbForm; $j++ ){
+                $stage -> addFormation($tableauFormations[$j-1]);
+                $tableauFormations[$j-1]->addStage($stage);
+                $manager->persist($tableauFormations[$j-1]);
+            }
+            
+            $manager->persist($stage);
+            $manager->persist($ent);
+            
+        }
+
+
         $manager->flush();
     }
 }
